397b2c3fe2e3de5abd2f54653ba70f7c686e4eb8
Rihan Badhon <rihanmiahbadon@gmail.com>
2025-04-15 21:35:43 -0700

    Controller Created

---
+++ Vehicle Rental System/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vehicle_Rental_System.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
