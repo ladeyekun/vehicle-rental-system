39c7a10a3cdd2c43cbdb99a3c4f8b8ee9291d100
Rihan Badhon <rihanmiahbadon@gmail.com>
2025-04-16 20:29:02 -0700

    create view updated

--- Vehicle Rental System.Model/ViewModel/CustomerViewModel.cs
+++ Vehicle Rental System.Model/ViewModel/CustomerViewModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Vehicle_Rental_System.Model.ViewModel
 {
     public class CustomerViewModel
     {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Customer.Gender CustomerGender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string DriversLicenseId { get; set; }
+        public List<int> SelectedReservationIds { get; set; }
+        public List<int> SelectedHistoryIds { get; set; }
+        public List<SelectListItem> AvailableReservations { get; set; }
+        public List<SelectListItem> AvailableHistories { get; set; }
     }
 }
