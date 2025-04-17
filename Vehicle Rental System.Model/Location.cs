68b0e68f919296c9d821a5f78095d9ba23e92a87
jiayimitt <jiayi_nie@outlook.com>
2025-04-16 19:23:24 -0500

    Location update with vehicle

--- Vehicle Rental System.Model/Location.cs
+++ Vehicle Rental System.Model/Location.cs
@@ -1,6 +1,11 @@
-﻿namespace Vehicle_Rental_System.Model {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicle_Rental_System.Model {
     public class Location {
         public int LocationId { get; set; }
+
+        [Required(ErrorMessage = "Location name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed {1} characters.")]
         public string Name { get; set; }
 
         public ICollection<Vehicle> Vehicles { get; set; }
--- Vehicle Rental System/Controllers/LocationController.cs
+++ Vehicle Rental System/Controllers/LocationController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
 using Vehicle_Rental_System.BLL;
 using Vehicle_Rental_System.Model;
 
@@ -7,84 +9,79 @@ namespace Vehicle_Rental_System.Web.Controllers
     public class LocationController : Controller
     {
         private readonly LocationService _locationService;
+        private readonly VehicleService _vehicleService;
 
-        public LocationController(LocationService locationService)
+        public LocationController(LocationService locationService, VehicleService vehicleService)
         {
             _locationService = locationService;
+            _vehicleService = vehicleService;
         }
 
-        // GET: /Location
         public IActionResult Index()
         {
             var locations = _locationService.GetAllLocations();
             return View(locations);
         }
 
-        // GET: /Location/Details/5
-        public IActionResult Details(int id)
+        [HttpGet]
+        public async Task<IActionResult> Create()
         {
-            var location = _locationService.GetLocationById(id);
-            if (location == null)
-                return NotFound();
-
-            return View(location);
-        }
-
-        // GET: /Location/Create
-        public IActionResult Create()
-        {
+            var vehicles = await _vehicleService.GetVehiclesAsync();
+            ViewBag.Vehicles = new SelectList(vehicles, "VehicleId", "Brand");
             return View();
         }
 
-        // POST: /Location/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Location location)
+        public async Task<IActionResult> Create([Bind("Name")] Location location, int? selectedVehicleId)
         {
             if (!ModelState.IsValid)
             {
+                var vehicles = await _vehicleService.GetVehiclesAsync();
+                ViewBag.Vehicles = new SelectList(vehicles, "VehicleId", "Brand", selectedVehicleId);
                 return View(location);
             }
 
             _locationService.CreateLocation(location);
+
+            if (selectedVehicleId.HasValue)
+            {
+                var vehicle = await _vehicleService.GetVehicleByIdAsync(selectedVehicleId.Value);
+                if (vehicle != null)
+                {
+                    vehicle.LocationId = location.LocationId;
+                    await _vehicleService.UpdateVehicleAsync(vehicle);
+                }
+            }
+
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: /Location/Edit/5
+        [HttpGet]
         public IActionResult Edit(int id)
         {
             var location = _locationService.GetLocationById(id);
-            if (location == null)
-                return NotFound();
-
+            if (location == null) return NotFound();
             return View(location);
         }
 
-        // POST: /Location/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(Location location)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(location);
-            }
-
+            if (!ModelState.IsValid) return View(location);
             _locationService.UpdateLocation(location);
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: /Location/Delete/5
+        [HttpGet]
         public IActionResult Delete(int id)
         {
             var location = _locationService.GetLocationById(id);
-            if (location == null)
-                return NotFound();
-
+            if (location == null) return NotFound();
             return View(location);
         }
 
-        // POST: /Location/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int id)
